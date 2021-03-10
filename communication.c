aeefc887713545ee3ed0f3db5aa61decff6285b9
Alyssa K. Anderson <f0046fv@plank.thayer.dartmouth.edu>
2021-03-10 02:02:16 -0500

    updated documentation

--- communication.c
+++ communication.c
@@ -1,144 +1,252 @@
-#include <stdlib.h>
-#include <stdbool.h>
-#include "string.h"
-#include "./support/message.h"
-#include "./player.h"
-#include "./game.h"
-#include "math.h"
-#include "./libcs50/hashtable.h"
-#include "communication.h"
-#include "./visibility.c"
+/* communication - function to communicate with client
+ *
+ * All communication done wiht players and spectators. 
+ * Responsible for networking, using message.h module
+ *
+ * Team: Nuggets
+ *
+ */
 
+#include <math.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include "./game.h"
+#include "./player.h"
+#include "./visibility.c"
+#include "communication.h"
+#include "./support/message.h"
+#include "./libcs50/hashtable.h"
+
+/************************ getNumDigits ***********************/
+/*
+ * Return the number of digits in the integer argument
+ * Usage: getNumDigits(rows)
+ */
 int getNumDigits(int a){
-    return a == 0 ? 1 : (int)(ceil(log10(a))+1);
+    return a == 0 ? 1 : (int)(ceil(log10(a)) + 1);
 }
 
-void quit(const addr_t addr, const char *reason){
-    char quitMessage[sizeof(char)*strlen(reason) + 6]; 
+/*************************** quit ****************************/
+/*
+ * Build and send a string message to quit the game
+ * See communication.h for detailed description.
+ */
+void quit(const addr_t addr, const char *reason) {
+    // initialize message
+    char quitMessage[sizeof(char) * strlen(reason) + 6]; 
+
+    // concatenate QUIT and reason to message
     sprintf(quitMessage, "QUIT %s", reason);
-    printf("%s\n", reason);  
+    printf("%s\n", reason);
+
     message_send(addr, quitMessage);
 }
 
-void quitGame(game_t *game, addr_t addr){
+/************************* quitGame **************************/
+/*
+ * Handle a player and spectator that quit the game
+ * See communication.h for detailed description.
+ */
+void quitGame(game_t *game, addr_t addr) {
     addr_t *spectatorAddr = game->spectatorAddr;
 
     if(spectatorAddr != NULL && message_eqAddr(addr, *spectatorAddr)){
-        // coming from a spectator
-        game->spectatorAddr = NULL;
+        // send quit message for a spectator
         quit(addr, "Thanks for Watching");
-    }else{
-        // coming from a player
+
+        // set the spectator address to null
+        game->spectatorAddr = NULL;         
+    } else {
+        // send quit message for a player
         quit(addr, "Thanks for Playing");
+
+        // decrease the current number of players in game
         game->playersJoined--;
     }
 }
 
+/************************** sendOK ***************************/
+/*
+ * Send okay message to player
+ * See communication.h for detailed description.
+ */
 void sendOK(player_t *player){
-    char OkMessage[5];
+    #define MESSAGESIZE 5
+    char OkMessage[MESSAGESIZE];
+
+    // concatenate OK and player ID to message
     sprintf(OkMessage, "OK %c", (char)(player->id + 'A'));
+
     message_send(player->addr, OkMessage);
 }
 
 
+/************************ sendGridInfo ***********************/
+/*
+ * Send grid maessage with rows and columns information
+ * See communication.h for detailed description.
+ */
 void sendGridInfo(game_t *game, addr_t addr){
-    int rows = game->rows;
-    int cols = game->cols;
+    int rows = game->rows;      // get number of rows from game
+    int cols = game->cols;      // get number of columns from game
+
+    // calculate the number of digits in rows and columns
     int numDigits = 6 + getNumDigits(rows) + getNumDigits(cols);
-    
-    char gridInfo[numDigits];
-    // Note to self: check for buffer overflow
+    char gridInfo[numDigits];   // create array for grid info
+
+    // TODO: check for buffer overflow
+    // concatenate GRID message with rows and columns
     sprintf(gridInfo, "GRID %d %d", rows, cols);
+
     message_send(addr, gridInfo);
 }
 
 
-// n is the amount of gold found
+// TODO: move to comm.h: n is the amount of gold found
 // if player == NULL and n = 0, then it is from a spectator
+/********************** sendGoldInfo *************************/
+/*
+ * Send gold maessage with amount of gold found
+ * See communication.h for detailed description.
+ */
 void sendGoldInfo(game_t *game, player_t *player, addr_t addr, int n){
-    
-    int p = player == NULL ? 0 : player->gold;
-    int r = game->TotalGoldLeft;
+    // if player is null, set p to 0, else set p to player's gold value
+    int p = (player == NULL) ? 0 : player->gold;
+    int r = game->TotalGoldLeft;    // r holds the amount of gold left
+
+    // calculate the number of digits in gold info
     int numDigits = 7 + getNumDigits(n) + getNumDigits(p) + getNumDigits(r);
-    
-    char goldInfo[numDigits];
+    char goldInfo[numDigits];   // create array for gold info
+
+    // concatenate GOLD message with 
+    // gold n just collected, p purse, and r remaining gold in game
     sprintf(goldInfo, "GOLD %d %d %d", n, p, r);
+
     message_send(addr, goldInfo);
 }
 
-// if player == NULL, then it is from spectator
+/*********************** sendDisplay *************************/
+/*
+ * Send display message with map information
+ * See communication.h for detailed description.
+ * TODO: loops don't differ between cases. Add more info or just keep 1 loop?
+ */
 void sendDisplay(game_t *game, player_t *player, addr_t addr){
-    int rows = game->rows;
-    int cols = game->cols;
+    int rows = game->rows;      // get number of rows from game
+    int cols = game->cols;      // get number of columns from game
     
-    char displayInfo[80 + (rows+1) * cols];
-    strcpy(displayInfo, "DISPLAY\n");
+    char displayInfo[8 + (rows+1) * cols];  // create array for display info message
+    strcpy(displayInfo, "DISPLAY\n");       // copy DISPLAY to info message
     
     if (player != NULL){
         // player
-        for(int i = 0; i<rows; i++){
-            strcat(displayInfo, player->visibility[i]);
+        for (int i = 0; i < rows; i++) {
+            // loop through rows, add to display info message with new line
+            strcat(displayInfo, game->map[i]);
             strcat(displayInfo, "\n");
         }
     } else {
         // spectator
-        for(int i = 0; i<rows; i++){
+        for(int i = 0; i < rows; i++) {
+            // loop through rows, add to display info message with new line
             strcat(displayInfo, game->map[i]);
             strcat(displayInfo, "\n");
         }
     }
+
     message_send(addr, displayInfo);
 }
 
+/*********************** sendGameOver ************************/
+/*
+ * Send game over message
+ * See communication.h for detailed description.
+ * TODO: need for two messages?
+ */
 void sendGameOver(game_t *game, addr_t addr){
-    
-    char *message = malloc( sizeof(char) * 500 );
+    #define MESSAGESIZE 700
+    // allocate memory and concatenate GAME OVER message
+    char *message = malloc(sizeof(char) * MESSAGESIZE);
     strcat(message, "GAME OVER:\n");
+    char message2[MESSAGESIZE]; // create array for message 2
+
     hashtable_iterate(game->players, message, create_message);
     printf("%s\n", message);
-    char message2[700];
-    strcpy(message2, message);
+
+    // copy message to message 2 and free message 
+    strcpy(message2, message);  
     free(message);
+
     hashtable_iterate(game->players, message2, broadcast);
+    
     // send message to spectator
-    if(game->spectatorAddr != NULL){
+    if (game->spectatorAddr != NULL){
         quit(*(game->spectatorAddr), message);
     }
 }
 
-// struct used solely for searching
+/************************ htSearch ***************************/
+/*
+ * Struct used solely for searching, used by getPlayerAddr funct
+ * See communication.h for detailed description.
+ * TODO: move typedefs to top of file, or group together
+ * TODO: combine struct with htSearch2?
+ */
 typedef struct htSearch {
     player_t *result;
     addr_t addr;
 } htSearch_t;
-
-player_t* getPlayerByAddr(game_t *game, const addr_t addr){
-    htSearch_t *obj = malloc(sizeof(htSearch_t));
-    obj->addr = addr;
-
-    hashtable_iterate(game->players, obj, find_player);
-    
-    //free(obj->addr);
-    player_t *result = obj->result;
-    free(obj); 
-    return result;
-}
-
-void find_player(void *arg, const char *key, void *item){
-    htSearch_t *search = (htSearch_t *) arg;
-    player_t *player = (player_t *) item;
-
-    if(message_eqAddr(search->addr, player->addr)){
-        search->result = player;
-    }
-}
 
 typedef struct htSearch2 {
     player_t *result;
     char car;
 } htSearch2_t;
 
+/********************* getPlayerByAddr ***********************/
+/*
+ * Return player at given address
+ * See communication.h for detailed description.
+ */
+player_t* getPlayerByAddr(game_t *game, const addr_t addr){
+    // create obj to store given address
+    htSearch_t *obj = malloc(sizeof(htSearch_t));
+    obj->addr = addr;
+
+    // iterate through game to find player at obj
+    hashtable_iterate(game->players, obj, find_player);
+    
+    // store result and free obj
+    player_t *result = obj->result;
+    free(obj);
+
+    return result;
+}
+
+/*********************** find_player *************************/
+/*
+ * Find player in game
+ * See communication.h for detailed description.
+ * TODO: update name convention to match other functions
+ * TODO: add comments
+ */
+void find_player(void *arg, const char *key, void *item){
+    htSearch_t *search = (htSearch_t *) arg;
+    player_t *player = (player_t *) item;
+
+    if (message_eqAddr(search->addr, player->addr)){
+        search->result = player;
+    }
+}
+
+/********************* getPlayerByChar ***********************/
+/*
+ * Return player with given character ID
+ * See communication.h for detailed description.
+ * TODO: remove comment
+ */
 player_t *getPlayerByChar(game_t *game, char c){
+    // create obh to store given char
     htSearch2_t *obj = malloc(sizeof(htSearch2_t));
     obj->car = c;
     obj->result = NULL;
@@ -146,29 +254,53 @@ player_t *getPlayerByChar(game_t *game, char c){
     hashtable_iterate(game->players, obj, find_player2);
     
     //free(obj->addr);
+    // store result and free obj
     player_t *result = obj->result;
     free(obj); 
+
     return result;
 }
 
+
+/********************** find_player2 *************************/
+/*
+ * Return player with given player character name
+ * See communication.h for detailed description.
+ * TODO: change name and name format to match
+ */
 void find_player2(void *arg, const char *key, void *item){
     htSearch2_t *search = (htSearch2_t *) arg;
+
+    // find and return player with matching ID
     if( search->car == ((player_t *) item)->letter){
         search->result = item;
     }
 }
 
+/******************** create_message *************************/
+/*
+ * Create a message with player information
+ * See communication.h for detailed description.
+ */
 void create_message(void *arg, const char *key, void *item){
-    char *message = (char *) arg;
     player_t *player = (player_t *) item;
-    char line[26];
+    char *message = (char *) arg;
+    char line[26];      // array to store message
+
+    // concatenate message with player letter and gold
     sprintf(line, "%c %10d %10s\n", player->letter, player->gold, key); 
     printf("%s", line);
     strcat(message, line);
 }
 
+/*********************** broadcast ***************************/
+/*
+ * Create broadcast message for a player quitting
+ * See communication.h for detailed description.
+ */
 void broadcast(void *arg, const char *key, void *item){
+    player_t *player = (player_t *) item;
     char* message = (char *) arg;
-    player_t *player = (player_t *) item;    
+
     quit(player->addr, message);
 }
